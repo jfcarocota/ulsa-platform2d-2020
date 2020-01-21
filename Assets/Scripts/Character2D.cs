201a749c366f79fecff3695e885e8a99a45b44ac
jfcarocota <jfcarocota@gmail.com>
2020-01-21 07:15:18 -0700

    basic library

--- Assets/Scripts/Character2D.cs
+++ Assets/Scripts/Character2D.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Platform2DUtils.GameplaySystem;
 
 public class Character2D : MonoBehaviour
 {
-    
+    void Start()
+    {
+        GameplaySystem.Hello2DGame();
+    }
 }
---
+++ Assets/Scripts/GameplaySystem.cs
@@ -0,0 +1,12 @@
+namespace Platform2DUtils.GameplaySystem
+{
+    using UnityEngine;
+    
+    public class GameplaySystem
+    {
+        public static void Hello2DGame()
+        {
+            Debug.Log("Hello 2D Game");
+        }
+    }
+}
