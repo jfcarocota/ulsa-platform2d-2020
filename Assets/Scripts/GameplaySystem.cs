2ceaa55fd1789dd30f286030aca340d67d359d86
jfcarocota <jfcarocota@gmail.com>
2020-01-21 07:35:15 -0700

    basic summary

--- Assets/Scripts/GameplaySystem.cs
+++ Assets/Scripts/GameplaySystem.cs
@@ -4,6 +4,9 @@ namespace Platform2DUtils.GameplaySystem
 
     public class GameplaySystem
     {
+        ///<summary>
+        /// Returns a Vector2 with Horizontal and Vertical axes.
+        ///</summary>
         public static Vector2 Axis
         {
             get => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
