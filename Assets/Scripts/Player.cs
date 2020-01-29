5ae956a92f087063864121e185b4cc3e627e8266
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2020-01-29 07:30:30 -0700

    gamemanager

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,8 +5,6 @@ using Platform2DUtils.GameplaySystem;
 
 public class Player : Character2D
 {
-    [SerializeField]
-    Score score;
 
     void FixedUpdate()
     {
@@ -37,7 +35,8 @@ public class Player : Character2D
         if(other.CompareTag("collectable"))
         {
             Collectable collectable = other.GetComponent<Collectable>();
-            score.AddPoints(collectable.Points);
+            Gamemanager.instance.Score.AddPoints(collectable.Points);
+            //score.AddPoints(collectable.Points);
             Destroy(other.gameObject);
         }
     }
