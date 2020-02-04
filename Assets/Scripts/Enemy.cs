648f94826838482f246192e33ec13f70ef4de6f6
jfcarocota <jfcarocota@gmail.com>
2020-02-04 07:55:54 -0700

    basic enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 3f;
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+    }
+}
