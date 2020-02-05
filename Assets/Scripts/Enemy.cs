16fc7a320f235bc3dcf6e07de15b9f09a0c6703b
jfcarocota <jfcarocota@gmail.com>
2020-02-05 07:53:56 -0700

    patrol basic

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,9 +6,23 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField]
     float moveSpeed = 3f;
+    [SerializeField]
+    float delay;
+    float timer;
+
+    [SerializeField]
+    Vector2 dir;
 
     void Update()
     {
-        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+        transform.Translate(dir * moveSpeed * Time.deltaTime);
+
+        timer +=  Time.deltaTime;
+        if(timer >= delay)
+        {
+            timer = 0f;
+            //dir = dir == Vector2.right ? Vector2.left : Vector2.right;
+            dir.x = dir.x > 0 ? -1 : 1;
+        }
     }
 }
